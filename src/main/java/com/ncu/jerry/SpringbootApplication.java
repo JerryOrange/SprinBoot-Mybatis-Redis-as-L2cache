81afaee7f229a054c879b04c4b997e269779f113
JerryOrange <563146556@qq.com>
2017-07-08 12:09:19 +0800

    spring引导类、配置类添加注释

--- src/main/java/com/ncu/jerry/SpringbootApplication.java
+++ src/main/java/com/ncu/jerry/SpringbootApplication.java
@@ -3,10 +3,11 @@ package com.ncu.jerry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  //开启组建扫描和自动配置
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
+	    //负责启动引导应用程序
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 }
