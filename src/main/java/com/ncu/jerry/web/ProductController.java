c0d66d342a199f4229e04c54df5564b08829f5e5
JerryOrange <563146556@qq.com>
2017-07-08 12:54:27 +0800

    修改MVC Controller中业务方法调用service中的方法

--- src/main/java/com/ncu/jerry/web/ProductController.java
+++ src/main/java/com/ncu/jerry/web/ProductController.java
@@ -2,6 +2,7 @@ package com.ncu.jerry.web;
 
 import com.ncu.jerry.dao.ProductDao;
 import com.ncu.jerry.entity.Product;
+import com.ncu.jerry.service.ProductService;
 import com.ncu.jerry.util.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,28 +14,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/product")
 public class ProductController {
     @Autowired
-    private ProductDao productMapper;
+    private ProductService productService;
 
     @GetMapping("/{id}")
-    public Product getProductInfo(
-            @PathVariable("id")
-                    Long productId) {
-        return productMapper.select(productId);
+    public Product getProductInfo( @PathVariable("id") Long productId) {
+        return productService.select(productId);
     }
 
     @PutMapping("/{id}")
     public Product updateProductInfo(
-            @PathVariable("id")
-                    Long productId,
-            @RequestBody
-                    Product newProduct) {
-        Product product = productMapper.select(productId);
+        @PathVariable("id") Long productId,
+        @RequestBody Product newProduct) {
+        Product product = productService.select(productId);
         if (product == null) {
             throw new ProductNotFoundException(productId);
         }
         product.setName(newProduct.getName());
         product.setPrice(newProduct.getPrice());
-        productMapper.update(product);
+        productService.update(product);
         return product;
     }
 }
